1a6f526c23a5eeaa930620e021c8ae7caa634796
Artur Cs <arturcamerasegat@gmail.com>
2023-04-21 16:30:32 -0300

    compatibility changes

--- data/API/__init__.py
+++ data/API/__init__.py
@@ -6,15 +6,14 @@ from os import path
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
+
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "hello"
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
-
     from .funcs import funcs
-
 
     app.register_blueprint(funcs, url_prefix='/')
 
@@ -22,7 +21,9 @@ def create_app():
 
     return app
 
+
 def create_database(app):
     if not path.exists('website/' + DB_NAME):
-        db.create_all(app=app)
+        with app.app_context():
+            db.create_all()
         print('database created')
