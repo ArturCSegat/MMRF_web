9e190c2508c90ffb7de8196dd5a9d879d2f71ec0
Artur Cs <arturcamerasegat@gmail.com>
2023-05-05 10:37:58 -0300

    refactor adn rewirte to improve readibilty and performance, spread into multiple JS files and change the mode sleection

--- scripts/mode_state_machine.js
+++ scripts/mode_state_machine.js
@@ -1,9 +1,10 @@
-// this will handle any clicks given to the map element and determine what to do with them
-// will also auto change modes without user input 
 import { get_mode, set_mode } from "./dom_elements.js"
 import { handle_click_create } from "./create_mode.js"
 import { handle_click_cut, get_empty_prop } from "./cut_mode.js"
 import { handle_click_branch } from "./branch_mode.js"
+
+// this will handle any clicks given to the map element and determine what to do with them
+// will also auto change modes without user input 
 
 
 async function click_state_machine(position, postes, square_limits, map){
