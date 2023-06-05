6855b714dbe609792de985317d546c8f09e3c323
Artur Cs <arturcamerasegat@gmail.com>
2023-06-05 19:36:17 -0300

    changes to reflect server_rewrite, mostly tested

--- scripts/dom_elements.js
+++ scripts/dom_elements.js
@@ -1,5 +1,16 @@
 // FILE FOR OPERATIONS REGARDING DOM ELEMENTS
 
+// this functin will also handle setting the session cookie for some reason 
+// TODO: fix this lates it is stupid
+async function read_files(){
+    let data = new FormData();
+    const input = document.getElementById("file_entry");
+    data.append("rede", input.files[0]);
+    await fetch("http://localhost:1337/upload_csv/", {method: "POST", credentials: "include", body: data})
+    window.location.reload()
+}
+
+
 function get_limit(){
     const limit_obj = document.getElementById("limit")
     return parseFloat(limit_obj.innerHTML)
@@ -14,18 +25,18 @@ function get_mode(){
 
 function set_mode(mode){
     mode = mode.toUpperCase()
-    if (!["BRANCH", "CREATE", "CUT"].includes(mode)){
+    if (!["BRANCH", "CUT"].includes(mode)){
         mode = "BRANCH"
     }
     document.getElementById("mode").innerText = mode
 }
 
 function handle_mode_click(){
-    const modes = ["BRANCH", "CREATE", "CUT"];
+    const modes = ["BRANCH", "CUT"];
     const current_mode = get_mode();
     for (let i = 0; i<modes.length; i++){
         if (modes[i] === current_mode){
-            if (i === 2){ // 2 is the lenght of modes - 1
+            if (i === 1){ // 2 is the lenght of modes - 1
                 i = -1 // must be -1 because it will be incremanted soon
             }
             set_mode(modes[i+1])
@@ -47,4 +58,5 @@ function hide_download_button(){
 }
 
 window.handle_mode_click = handle_mode_click;
+window.read_files = read_files;
 export { get_limit, get_mode, set_mode, show_download_button, hide_download_button }
--- scripts/request_builder.js
+++ scripts/request_builder.js
@@ -2,12 +2,8 @@ async function request_builder(end_point, method, payload){
     const host = "http://localhost:1337"
     const url = host + end_point;
     console.log(url, method, JSON.stringify(payload))
-    const request = await fetch(url, {method:method,  headers:{'Content-Type': 'application/json', "Access-Control-Allow-Origin" : "*"}, body:JSON.stringify(payload)})
+    const request = await fetch(url, {method:method,  credentials:"include", body:JSON.stringify(payload)})
     const data = await request.json()
-    for (let prop in data){
-        let valid_data = data[prop]
-        console.log(valid_data)
-        return valid_data
-    }
+    return data
 }
 export { request_builder }
